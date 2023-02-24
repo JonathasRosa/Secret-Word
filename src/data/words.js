a86346f6fc54362a24ab6e6b1e225b9bb88b99c0
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-02-24 08:10:15 -0300

    Update Secret-word com React

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Atena", "Parabrisa", "Step", "Acento", "Cambio", "Direção"],
+    fruta: ["Banana", "Maça", "Laranja", "Limão", "Tangerina", "Mamão", "Abacaxi", "Acerola", "Melancia", "Caja"],
+    corpo: ["Braço", "Perna", "Pescoço", "Cerebro", "Olhos", "Orelha", "Mão", "Cotovelo", "Tornrzelo", "Peito"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Memória", "Estabilizador", "HD", "SSD"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Bootstrap", "CSS", "HTML"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Macarrão", "Peixe", "Legumes"],
+};
